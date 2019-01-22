f7ca60c6c2c334fd76c15d20ee10322fac2a29df
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2019-01-22 10:47:51 -0800

    create Xcode project

---
+++ PathKit/PathKit/PathKit.h
@@ -0,0 +1,19 @@
+//
+//  PathKit.h
+//  PathKit
+//
+//  Created by Jeremie Benhamron on 2019-01-22.
+//  Copyright © 2019 jabt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PathKit.
+FOUNDATION_EXPORT double PathKitVersionNumber;
+
+//! Project version string for PathKit.
+FOUNDATION_EXPORT const unsigned char PathKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathKit/PublicHeader.h>
+
+
