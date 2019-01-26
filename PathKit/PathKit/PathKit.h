5ddace29e10dba30cd2471d8719221806c8ea456
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2019-01-25 23:24:06 -0800

    create Xcode project (#2)
    * create Xcode project
    
    * review

---
+++ PathKit/PathKit/PathKit.h
@@ -0,0 +1,18 @@
+//// Copyright © 2019 JABT Labs. All rights reserved.
+//
+// This file is part of PathKit. The full PathKit copyright notice,
+// including terms governing use, modification, and redistribution, is
+// contained in the file LICENSE at the root of the source code distribution
+// tree.
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PathKit.
+FOUNDATION_EXPORT double PathKitVersionNumber;
+
+//! Project version string for PathKit.
+FOUNDATION_EXPORT const unsigned char PathKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathKit/PublicHeader.h>
+
+
