e1df610c3539078de135cbb901fb77b704d64796
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2019-02-22 19:00:48 -0800

    Use normalize angles to decide whether two angles are equal.

--- PathKit/Sources/Angle.swift
+++ PathKit/Sources/Angle.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct Angle: Equatable {
+public struct Angle {
 
     public static let π = Angle(radians: Double.pi)
     public static let τ = Angle(radians: 2 * Double.pi)
@@ -29,14 +29,14 @@ public struct Angle: Equatable {
     }
 
     /// True if the angle is positive, false otherwise.
-    public var isPositive: Bool {
-        return 0.0 < radians
+    public var isNonNegative: Bool {
+        return 0.0 <= radians
     }
 
     /// A positive normalized angle between 0 and 2π.
     public var positiveNormalized: Angle {
         let angle = Angle(radians: radians.truncatingRemainder(dividingBy: Angle.τ.radians))
-        return isPositive ? angle : angle + Angle.τ
+        return isNonNegative ? angle : angle + Angle.τ
     }
 
     /// Returns the angular distance between two angles.
@@ -126,14 +126,41 @@ public func tan(_ angle: Angle) -> Double {
     return tan(angle.radians)
 }
 
+extension Angle: Equatable {
+    /// Returns true if the positive normalized value of the first angle is equal to the
+    /// positive normalized value of the second angle, and false otherwise.
+    ///
+    /// - Note: Both angles are normalized before
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    public static func == (lhs: Angle, rhs: Angle) -> Bool {
+        return lhs.positiveNormalized.radians == rhs.positiveNormalized.radians
+    }
+}
+
 extension Angle: Comparable {
+    /// Returns true if the positive normalized value of the first angle is closer to 0 than the
+    /// positive normalized value of the second angle, and false otherwise.
+    ///
+    /// - Note: Both angles are normalized before
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func < (lhs: Angle, rhs: Angle) -> Bool {
-        return lhs.radians < rhs.radians
+        return lhs.positiveNormalized.radians < rhs.positiveNormalized.radians
     }
 }
 
 extension Angle: ApproximatelyEquatable {
+    /// Returns true if the distance between two angles is bounded by a given accuracy, and false otherwise.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    /// - Precondition: The provided `accuracy` must be a positive angle.
     public static func equal(_ lhs: Angle, _ rhs: Angle, accuracy: Angle) -> Bool {
+        precondition(accuracy.radians > 0)
         return distance(lhs, rhs) <= accuracy
     }
 }
